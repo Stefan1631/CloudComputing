9be85d39168ab549f14ed1b157ebd3223f795b41
Stefan Manjescu <stefanmanjescu@gmail.com>
2026-05-14 15:34:15 +0300

    mic refactor

--- proiect_cloud/main.py
+++ proiect_cloud/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from firestoreService.datastoreService import DatastoreService
+from datastoreService.datastoreService import DatastoreService
 from models.models import *
 
 app = FastAPI(
